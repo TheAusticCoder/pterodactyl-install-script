aa318cc6e20fc3de804be58f506281b9f02ae5a7
TheAusticCoder <53130379+TheAusticCoder@users.noreply.github.com>
2024-08-02 11:08:55 -0700

    Add files via upload

---
+++ pterodactyl install-works.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+apt -y install software-properties-common curl apt-transport-https ca-certificates gnupg
+LC_ALL=C.UTF-8 add-apt-repository -y ppa:ondrej/php
+curl -fsSL https://packages.redis.io/gpg | gpg --dearmor -o /usr/share/keyrings/redis-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://packages.redis.io/deb $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/redis.list
+curl -sS https://downloads.mariadb.com/MariaDB/mariadb_repo_setup | bash
+apt update
+apt -y install php8.1 php8.1-common,cli,gd,mysql,mbstring,bcmath,xml,fpm,curl,zip mariadb-server nginx tar unzip git redis-server
+curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+mkdir -p /var/www/pterodactyl
+cd /var/www/pterodactyl
+curl -Lo panel.tar.gz https://github.com/pterodactyl/panel/releases/latest/download/panel.tar.gz
+tar -xzvf panel.tar.gz
+chmod -R 755 storage/* bootstrap/cache/
+cp .env.example .env
+composer install --no-dev --optimize-autoloader
+php artisan key:generate --force
+php artisan p:environment:setup
+php artisan p:environment:database
+php artisan migrate --seed --force
+php artisan p:user:make
+chown -R www-data:www-data /var/www/pterodactyl/*
+
+cat <<EOT > /etc/systemd/system/pteroq.service
+[Unit]
+Description=Pterodactyl Queue Worker
+After=redis-server.service
+
+[Service]
+User=www-data
+Group=www-data
+Restart=always
+ExecStart=/usr/bin/php /var/www/pterodactyl/artisan queue:work --queue=high,standard,low --sleep=3 --tries=3
+StartLimitInterval=180
+StartLimitBurst=30
+RestartSec=5s
+
+[Install]
+WantedBy=multi-user.target
+EOT
+
+systemctl enable --now redis-server
+systemctl enable --now pteroq.service
